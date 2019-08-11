2e1d6718d72e43533977cf9ee1e953231700ca18
myshacodes <52944820+myshacodes@users.noreply.github.com>
2019-08-11 15:34:17 -0500

    hangman

---
+++ app.js
@@ -0,0 +1,8 @@
+var words = ["monkey", "pyramid", "window", "girrafe", "ocean", "friend"];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+var answerArray = [];
+for (var i = 0; i < array.length; i++) {
+  const element = array[i];
+}
